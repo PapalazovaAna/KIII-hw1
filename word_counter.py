6106d6515d7f63fc5d65fb83ef5b61d633b29ec6
Ana Papalazova <anapapalazova@gmail.com>
2025-03-02 22:36:12 +0100

    Updated word counter and shopping list

--- word_counter.py
+++ word_counter.py
@@ -3,8 +3,15 @@ def count_words(text):
     return len(words)
 
 
+def count_letters(text):
+    return len(text.replace(" ", ""))
+
+
 if __name__ == "__main__":
     user_text = input("Внеси некој текст: ")
 
     word_count = count_words(user_text)
     print(f"Текстот содржи {word_count} зборови.")
+
+    letter_count = count_letters(user_text)
+    print(f"Текстот содржи {letter_count} букви.")
